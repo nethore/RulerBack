84c4432cbbac2cb62ac13c087feff27b8440df37
nethore <nikolas.ethore@gmail.com>
2017-01-12 09:57:57 +0100

    Push Jeudi 12 Matin

--- resources/views/ruler/index.blade.php
+++ resources/views/ruler/index.blade.php
@@ -16,6 +16,16 @@
     <script src="{{ asset('bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js') }}" charset="utf-8"></script>
     <script type="text/javascript" src="{{ asset('dist/js/app.min.js') }}"></script>
 
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-90215313-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
